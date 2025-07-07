1f38d7fafdb05d1f198cf7560265e8c1753a5e78
TheFallenUSER <eduardopea616@gmail.com>
2025-07-07 16:00:58 -0500

    Experiencia 03 completada

---
+++ Experiencia_02/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, ColorSensor, UltrasonicSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+ev3 = EV3Brick()
+murci = UltrasonicSensor(Port.S1)
+mA = Motor(Port.A)
+mD = Motor(Port.D)
+ev3.speaker.beep()
+
+while True:
+    distancia = murci.distance()
+    mA.run(300)
+    mD.run(300)
+    ev3.light.on(Color.GREEN)
+    if distancia <= 230:
+        ev3.light.on(Color.RED)
+        wait(300)
+        ev3.speaker.beep(frequency=500, duration=100)
+        ev3.light.off()
+        ev3.screen.print("Obstáculo detectado:")
+        print(distancia/10, "cm")
+        wait(200)
+        mA.brake()
+        mD.brake()
+        wait(500)
+        mA.run(700)
+        wait(630)
+
