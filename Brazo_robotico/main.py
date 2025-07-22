51284e9d9f155619230df2f6d5600d81f6bb532f
TheFallenUSER <eduardopea616@gmail.com>
2025-07-22 10:20:25 -0500

    Brazo robótico terminado(temporal)

--- Brazo_robotico/main.py
+++ Brazo_robotico/main.py
@@ -3,6 +3,7 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.parameters import Port, Stop, Direction
 from pybricks.tools import wait
+from pybricks.media.ev3dev import SoundFile, ImageFile
 
 ev3 = EV3Brick()
 gancho_motor = Motor(Port.A)
@@ -15,7 +16,7 @@ codo_sensor = ColorSensor(Port.S3)
 
 codo_motor.run_time(-30, 1000)
 codo_motor.run(15)
-while codo_sensor.reflection() < 32:
+while codo_sensor.reflection() < 35:
     wait(10)
 codo_motor.reset_angle(0)
 codo_motor.hold()
@@ -30,34 +31,29 @@ gancho_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
 gancho_motor.reset_angle(0)
 gancho_motor.run_target(200, -90)
 
-
 def robot_agarra(position):
     base_motor.run_target(60, position)
     codo_motor.run_target(60, -40)
     gancho_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=50)
+    ev3.speaker.play_file(SoundFile.CLICK)
     codo_motor.run_target(60, 0)
-
 
 def robot_suelta(position):
     base_motor.run_target(60, position)
     codo_motor.run_target(60, -40)
     gancho_motor.run_target(200, -90)
     codo_motor.run_target(60, 0)
+    ev3.speaker.play_file(SoundFile.GENERAL_ALERT)
 
 for i in range(3):
     ev3.speaker.beep()
     wait(100)
+ev3.speaker.play_file(SoundFile.ACTIVATE)
 
-IZQUIERDA = 160
+IZQUIERDA = 195
 MEDIO = 100
-DERECHA = 40
+DERECHA = 20
 
 while True:
     robot_agarra(IZQUIERDA)
-    robot_suelta(MEDIO)
-
-    robot_agarra(DERECHA)
-    robot_suelta(IZQUIERDA)
-
-    robot_agarra(MEDIO)
     robot_suelta(DERECHA)
